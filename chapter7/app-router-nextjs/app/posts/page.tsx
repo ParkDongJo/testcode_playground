1486953a214b848dc324df98f98387c2602326e2
ParkDongJo <pdj10211@gmail.com>
2025-11-22 23:06:14 +0900

    feat: 챕터 9 테스트 예제 코드 작성

--- chapter7/app-router-nextjs/app/posts/page.tsx
+++ chapter7/app-router-nextjs/app/posts/page.tsx
@@ -17,9 +17,7 @@ export default function Blog() {
     <h1>Posts</h1>
     <label htmlFor="title">Title</label>
     <input type="text" placeholder="title" aria-label="title" />
-    <button onClick={() => {
-      setPosts([...posts, { id: posts.length + 1, title: title, body: body, userId: 1 }]);
-    }}>Add Post</button>
+    <button type="submit">검색</button>
     <ul>
       {posts.map((post) => (
         <li key={post.id}>{post.title} by {post.userId}</li>
